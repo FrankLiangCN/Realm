d04c15feddc867e6981757dc8e6c019372c43241
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-01-11 19:23:14 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -7,7 +7,7 @@ systemctl reset-failed
 rm /opt/realm/realm
 mkdir -p /opt/realm
 cd /opt/realm
-uname -m | grep -qi aarch64 && oarch=arm-unknown-linux-gnu || oarch=x86_64-unknown-linux-gnu;
+uname -m | grep -qi oarch=x86_64-unknown-linux-gnu || aarch64 && oarch=arm-unknown-linux-gnu;
 wget "$(curl -s https://api.github.com/repos/zhboner/realm/releases/latest|grep -i "browser_download_url.*${oarch}"|awk -F '"' '{print $(NF-1)}')";
 tar -xzvf ./realm*.tar.gz
 chmod +x ./realm

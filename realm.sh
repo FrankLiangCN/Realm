1ed46c40effd076aaa50dff676a17edfe19b227b
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-01-12 00:29:36 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -11,6 +11,7 @@ uname -m | grep -qi x86_64 && oarch=x86_64-unknown-linux-gnu || oarch=aarch64-un
 wget "$(curl -s https://api.github.com/repos/zhboner/realm/releases/latest|grep -i "browser_download_url.*${oarch}"|grep -i "gnu.tar.gz"|awk -F '"' '{print $(NF-1)}')";
 tar -xzvf ./realm*.tar.gz
 chmod +x ./realm
+rm ./realm*.tar.gz -f
 wget --no-check-certificate -P /etc/systemd/system https://raw.githubusercontent.com/FrankLiangCN/Realm/main/realm.service
 wget --no-check-certificate -P /opt/realm https://raw.githubusercontent.com/FrankLiangCN/Realm/main/config.toml
 cd /root

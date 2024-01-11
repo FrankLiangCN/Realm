c51789fe1aa41c9c6697a2354613c3da36d378f0
FrankLiangCN <111636925+FrankLiangCN@users.noreply.github.com>
2024-01-11 19:12:51 +0800

    Create realm.sh

---
+++ realm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+systemctl stop realm
+systemctl disable realm
+rm /etc/systemd/system/realm.service -f
+systemctl daemon-reload
+systemctl reset-failed
+rm /opt/realm/realm
+mkdir -p /opt/realm
+cd /opt/realm
+uname -m | grep -qi aarch64 && oarch=arm-unknown-linux-gnu || oarch=x86_64-unknown-linux-gnu;
+wget "$(curl -s https://api.github.com/repos/zhboner/realm/releases/latest|grep -i "browser_download_url.*${oarch}"|awk -F '"' '{print $(NF-1)}')";
+tar -xzvf ./realm*.tar.gz
+chmod +x ./realm
